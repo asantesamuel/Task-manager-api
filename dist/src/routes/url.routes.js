f35cbfba0636919953382a106c4c44ff757b6301
Samuel Boakye Asante <asanteboakyesamuel@gmail.com>
2026-01-23 15:58:14 +0000

    chore: clean tracked files and apply gitignore

--- dist/src/routes/url.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const url_controller_1 = require("../controllers/url.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.post("/shorten", auth_middleware_1.protect, url_controller_1.createShortUrl);
-router.get("/:code", url_controller_1.redirectUrl);
-exports.default = router;
--- dist/src/utils/generateCode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateCode = void 0;
-const generateCode = () => {
-    return Math.random().toString(36).substring(2, 8);
-};
-exports.generateCode = generateCode;
